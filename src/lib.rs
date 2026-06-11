cefa2153253997baa9e115afec4c20054bce713b
indium114 <156162907+indium114@users.noreply.github.com>
2026-06-11 20:18:33 +0200

    feat: the rest of the owl
    also, removed prefixes and do everything inline

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,51 @@
 use colored::*;
 use std::fmt;
 
-mod prefixes;
+// MARK: reusable print function
+fn generic_print(prefix: ColoredString, text: impl fmt::Display) {
+    println!("{prefix} {text}")
+}
+fn generic_err(prefix: ColoredString, text: impl fmt::Display) {
+    eprintln!("{prefix} {text}")
+}
 
-fn generic_print(prefix: ColoredString, msg: impl fmt::Display) {
-    println!("{prefix} {msg}")
+// MARK: actual log thingies.
+pub fn ok(text: impl fmt::Display) {
+    generic_print("[ok]".green(), text);
+}
+
+pub fn warn(text: impl fmt::Display) {
+    generic_print("[warn]".yellow(), text);
+}
+
+pub fn err(text: impl fmt::Display) {
+    generic_err("[err]".red(), text);
+}
+
+pub fn sync(text: impl fmt::Display) {
+    generic_print("sync".blue(), text)
+}
+
+pub fn update(text: impl fmt::Display) {
+    generic_print("[update]".magenta(), text);
+}
+
+pub fn log(text: impl fmt::Display) {
+    generic_print("[log]".black().bold(), text);
+}
+
+pub fn clean(text: impl fmt::Display) {
+    generic_print("[clean]".cyan(), text);
+}
+
+pub fn add(text: impl fmt::Display) {
+    generic_print("[add]".magenta().bold(), text);
+}
+
+pub fn hint(text: impl fmt::Display) {
+    generic_print("[hint]".cyan().bold(), text);
+}
+
+pub fn query(text: impl fmt::Display) {
+    generic_print("[query]".yellow().bold(), text);
 }
